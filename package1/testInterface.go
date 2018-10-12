50aafa195232aafbf329ca43dac2c0143474c0ea
gongyao <825612127@qq.com>
2018-10-12 16:40:12 +0800

    [提交代码]

--- package1/testInterface.go
+++ package1/testInterface.go
@@ -2,13 +2,14 @@ package package1
 
 import (
 	"fmt"
-	"math/rand"
+	"strings"
 	"sync"
 	"time"
 )
 
 type (
 	push_driver chan interface{}
+	filt func(v interface{}) bool
 )
 //
 type Pusher struct {
@@ -34,15 +35,36 @@ func p(v interface{})  {
 	}
 }
 
+func IsShanxi(filt2 filt, v interface{}) string {
+	switch v.(type) {
+	case string:
+		if filt2 != nil && filt2(v) {
+			return "Yes"
+		}
+		return "OH, No"
+	default:
+		return "No"
+	}
+}
+
 func Test101202()  {
 
-	r := rand.New(rand.NewSource(time.Now().Unix()))
+	//r := rand.New(rand.NewSource(time.Now().Unix()))
+	//
+	//sl := [10]int{}
+	//for i := 0; i < 10; i++ {
+	//	sl[i] = r.Intn(1000)
+	//	//sl = append(sl, r.Intn(1000))
+	//}
+	//
+	//fmt.Println(len(sl))
 
-	sl := [10]int{}
-	for i := 0; i < 10; i++ {
-		sl[i] = r.Intn(1000)
-		//sl = append(sl, r.Intn(1000))
-	}
+	a := IsShanxi(func(v interface{}) bool {
+		if v == nil {
+			return false
+		}
+		return strings.Contains(v.(string), "山西")
+	}, "我是中国山西人")
 
-	fmt.Println(len(sl))
+	fmt.Println(a)
 }
