c3bd49a30fc2c49f1b1407fdfc6a0a9fe381f4df
gongyao <825612127@qq.com>
2018-11-14 18:42:22 +0800

    [提交代码]

--- mianshi/main.go
+++ mianshi/main.go
@@ -1,13 +1,12 @@
 package main
 
-import (
-	"mygo2/mianshi/funcTest"
-)
+import "mygo2/mianshi/test1014"
 
 func main()  {
-	funcTest.Test2()
+	//funcTest.Test2()
 	//panicTest.Test()
 	//funcTest.MyTest()
 	//fmt.Println("Hello World!")
+	test1014.StringMethod()
 }
 
